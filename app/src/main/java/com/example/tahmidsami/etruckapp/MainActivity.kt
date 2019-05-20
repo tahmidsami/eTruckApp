591cfcca90b1a50ee8f557537b04713819e83ad0
Tahmid Sami <mtsami7264@gmail.com>
2019-05-20 22:32:43 +0600

    Added map on map button

--- app/src/main/java/com/example/tahmidsami/etruckapp/MainActivity.kt
+++ app/src/main/java/com/example/tahmidsami/etruckapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.tahmidsami.etruckapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonMap.setOnClickListener{ //Google map button
+            var i = Intent(this, MapsActivity::class.java)
+            i.putExtra("latitude","23.77")
+            i.putExtra("longitude","90.42")
+            startActivity(i)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/tahmidsami/etruckapp/MapsActivity.kt
@@ -0,0 +1,36 @@
+package com.example.tahmidsami.etruckapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import android.support.v4.app.FragmentActivity
+
+
+class MapsActivity : FragmentActivity(), OnMapReadyCallback {
+
+    private var mMap: GoogleMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        val location = LatLng(23.77, 90.42)
+        mMap!!.addMarker(MarkerOptions().position(location).title("Marker in my location"))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(location))
+    }
+}
